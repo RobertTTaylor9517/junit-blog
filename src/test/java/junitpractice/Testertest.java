8e76541b844ca6799d3b35c2ed85faa7fdae0ae5
robert <robt15@hotmail.com>
2020-05-28 23:05:29 -0400

    Finished Tests

--- src/test/java/junitpractice/Testertest.java
+++ src/test/java/junitpractice/Testertest.java
@@ -1,14 +1,75 @@
 package junitpractice;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Enclosed.class)
 public class Testertest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	
+	public static class additonAndReverseTest {
+		
+		Tester testerObject = new Tester();
+		
+		@Test
+		public void test_additionTest() {
+			assertThat(testerObject.addition(2, 3), is(5));
+		}
+		
+		@Test
+		public void test_reverseStringTest() {
+			String inputString = "the";
+			
+			assertThat(testerObject.reverse(inputString), is("eht"));
+		}
 	}
+	
+	
+	
+	public static class equalsTwoTest {
+		Tester testerObject = new Tester();
+		
+		@Test
+		public void test_equalsTwoTrue() {
+			assertTrue(testerObject.equalsTwo(2));
+		}
+		
+		@Test
+		public void test_equalsTwoFalse() {
+			assertFalse(testerObject.equalsTwo(3));
+		}
+	}
+	
+	@RunWith(Parameterized.class)
+	public static class sortTest{
+		Tester testerObject = new Tester();
+		int[] expectedData = {1,3,4,6,8};
+		
+		@Parameters
+		public static int[][] data() {
+			int expected[][] = {{1,4,6,3,8}, {35, 68, 32, 45}};
+			
+			return expected;
+		}
+		
+		private int[] input;
 
+		public sortTest(int[] input) {
+			super();
+			this.input = input;
+		}
+		
+		@Test
+		public void test_sorter() {
+			assertArrayEquals(expectedData, testerObject.sortArray(input));
+		}
+		
+	}
+	
 }
